df62cba6cf4845a19b064a72e2d0583ac8a7c881
Sebas2007 <sebastorregroza6@gmail.com>
2026-03-23 17:25:12 -0500

    feat(setup): configure Astro with React, Tailwind and Prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+}
