a290a3860b8482510b2d5ffe6bdebef021d6db56
Sebas2007 <sebastorregroza6@gmail.com>
2026-03-23 18:25:04 -0500

    fix(config): update allowedHosts configuration and add @nurodev/astro-bun dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
+import bun from '@nurodev/astro-bun'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'server', // indica que no es solo static
+  adapter: bun(),
 
   vite: {
     plugins: [tailwindcss()],
     preview: {
-      allowedHosts:true
+      allowedHosts: true,
     },
   },
 })
